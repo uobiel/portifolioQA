465b4b1e81034986ac46df01b82877ef1a94ac28
uobiel <gabrielantunesabc@gmail.com>
2024-04-22 22:49:16 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const sessaoDois = document.querySelector("#sessao-dois");
 const cards = document.querySelectorAll(".cards");
 const sections = document.querySelectorAll(".hidden");
+const contactElements = {
+    linkedin: document.querySelector(".linkedin"),
+    whatsapp: document.querySelector(".whatsapp"),
+    email: document.querySelector(".email")
+}
+
+contactElements.linkedin.addEventListener('click', () => acessaSite("www.google.com.br"));
+
+const acessaSite = (url) => {
+    window.location.href = url;
+}
 
 const myObserver = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+        } 
     });
 });
 
